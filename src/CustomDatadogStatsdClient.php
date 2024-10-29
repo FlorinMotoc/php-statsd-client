487012e3ed60810ef6ed751ed709a5d864236772
Florin Motoc <dev@mflorin.com>
2024-10-29 13:52:22 +0200

    workaround for cases when $_ENV does not contain .env variables

--- src/CustomDatadogStatsdClient.php
+++ src/CustomDatadogStatsdClient.php
@@ -12,27 +12,45 @@ class CustomDatadogStatsdClient extends DatadogStatsdClient
             'disable_telemetry' => true,
         ];
 
-        if (!empty($_ENV['STATSD_CLIENT_CUSTOM_DATADOG_HOST'])) {
-            $config['host'] = $_ENV['STATSD_CLIENT_CUSTOM_DATADOG_HOST'];
+        if (!empty($this->getFromEnv('STATSD_CLIENT_CUSTOM_DATADOG_HOST'))) {
+            $config['host'] = $this->getFromEnv('STATSD_CLIENT_CUSTOM_DATADOG_HOST');
         }
 
-        if (!empty($_ENV['STATSD_CLIENT_CUSTOM_DATADOG_PORT'])) {
-            $config['port'] = $_ENV['STATSD_CLIENT_CUSTOM_DATADOG_PORT'];
+        if (!empty($this->getFromEnv('STATSD_CLIENT_CUSTOM_DATADOG_PORT'))) {
+            $config['port'] = $this->getFromEnv('STATSD_CLIENT_CUSTOM_DATADOG_PORT');
         }
 
-        if (!empty($_ENV['STATSD_CLIENT_CUSTOM_DATADOG_LOCAL_HOSTNAME'])) {
-            $config['global_tags'] = ['host' => $_ENV['STATSD_CLIENT_CUSTOM_DATADOG_LOCAL_HOSTNAME']];
+        if (!empty($this->getFromEnv('STATSD_CLIENT_CUSTOM_DATADOG_LOCAL_HOSTNAME'))) {
+            $config['global_tags'] = ['host' => $this->getFromEnv('STATSD_CLIENT_CUSTOM_DATADOG_LOCAL_HOSTNAME')];
         } else {
             $config['global_tags'] = ['host' => gethostname()];
         }
 
         // custom tags
         $customTagsPrefix = 'STATSD_CLIENT_CUSTOM_DATADOG_TAGS_CUSTOM_';
-        foreach (array_filter($_ENV, fn($key) => str_starts_with($key, $customTagsPrefix), ARRAY_FILTER_USE_KEY) as $key => $value) {
+        foreach (array_filter($this->getAllEnv(), fn($key) => str_starts_with($key, $customTagsPrefix), ARRAY_FILTER_USE_KEY) as $key => $value) {
             $key = strtolower(str_replace($customTagsPrefix, '', $key));
             $config['global_tags']["c_$key"] = $value;
         }
 
-        $this->client = new DogStatsd($config);
+        parent::__construct(new DogStatsd($config));
+    }
+
+    private function getFromEnv(string $key)
+    {
+        if (function_exists('env')) {
+            return env($key);
+        } else {
+            return $_ENV[$key] ?? '';
+        }
+    }
+
+    private function getAllEnv()
+    {
+        if (class_exists('\Dotenv\Dotenv') && function_exists('base_path')) {
+            return \Dotenv\Dotenv::createArrayBacked(base_path())->load();
+        } else {
+            return $_ENV;
+        }
     }
 }
